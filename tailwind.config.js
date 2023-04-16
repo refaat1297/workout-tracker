af51389e22eaef8060f6c30385acf0e00141960f
refaat1297 <a.refaat1297@gmail.com>
2023-04-16 15:47:27 +0200

    add router & supabase

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,30 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    extend: {
+      colors: {
+        "at-light-green": "#44ba9f",
+        "at-green": "#286d5d",
+        "light-grey": "#f1f1f1",
+      },
+    },
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"],
+    },
+    flex: {
+      "1": "1 1 0%",
+      "2": "2 2 0%",
+    },
+    container: {
+      center: true,
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
